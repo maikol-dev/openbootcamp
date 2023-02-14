4d7e5a4c5d5a761dafb9e204f76018236575c8fe
Maikol Castellano <56364360+maikol-dev@users.noreply.github.com>
2023-02-13 23:07:54 -0400

    Cargado ejercicio 2

---
+++ basico-js/ejercicio2-listas.js
@@ -0,0 +1,16 @@
+const yo = {
+    nombre: "Maikol Castellano",
+    edad: 25,
+    developer: false,
+    fecha: new Date(1998, 01,25),
+    libro: {
+        titulo: "Padre Rico, Padre Pobre",
+        autor: "Robert Kiyosaki",
+        fecha: new Date(2016, 02, 04),
+        url: "https://www.unpa.edu.mx/~blopez/algunosLibros/Padre-Rico-Padre-Pobre.pdf"
+    }
+
+}
+
+
+console.log(yo.libro.autor);
